c4bda8c1d0ca6a489c80dece584b8f1e6be5c4d6
Nicolás Galdámez <nicogaldamez@gmail.com>
2016-07-11 07:47:46 -0300

    N+1 query in vehicle model

--- app/forms/vehicle_filter.rb
+++ app/forms/vehicle_filter.rb
@@ -6,18 +6,18 @@ class VehicleFilter
     vehicles = Vehicle.all
 
     vehicles = vehicles.joins(:customer).where(
-        'customers.first_name LIKE ? OR customers.last_name LIKE ?', 
+        'customers.first_name LIKE ? OR customers.last_name LIKE ?',
           "%#{@name}%", "%#{@name}%"
         ) if @name.present?
 
     vehicles = vehicles.joins(:brand).where(
         'brands.name LIKE ?', "%#{@brand}%"
         ) if @brand.present?
-        
+
     vehicles = vehicles.joins(:vehicle_model).where(
         'vehicle_models.name LIKE ?', "%#{@vehicle_model}%"
         ) if @vehicle_model.present?
 
-    vehicles
+    vehicles.includes(:brand, :vehicle_model, :version)
   end
 end
