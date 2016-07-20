9b86afe74c7105eb36e4a6f094b44f7ae4674875
Nicolás Galdámez <nicogaldamez@gmail.com>
2016-07-20 12:16:59 -0300

    local_env added to gitignore

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -8,9 +8,18 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  unless Rails.env.development? || Rails.env.test?
+    datastore :file,
+              root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+              server_root: Rails.root.join('public')
+  else
+    datastore :s3,
+              bucket_name: ENV['BUCKET_NAME'],
+              access_key_id: ENV['S3_KEY'],
+              secret_access_key: ENV['S3_SECRET'],
+              url_scheme: 'https'
+  end
+
 end
 
 # Logger
