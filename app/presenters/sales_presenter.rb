80914071517cbd70f98e914d32d6c601f1265021
GONZALO <gonzagaldamez@gmail.com>
2016-08-29 18:13:47 -0300

    Add difference to sales totals and fix minor bugs

--- app/presenters/sales_presenter.rb
+++ app/presenters/sales_presenter.rb
@@ -14,13 +14,21 @@ class SalesPresenter
   end
 
   def total_price
-    number_to_currency(0) unless sales.any?
-    number_to_currency filter.call.sum(:price_in_cents)/10000
+    number_to_currency(filter.call.sum(:price_in_cents)/10000 || 0)
   end
 
   def total_cash
-    number_to_currency(0) unless sales.any?
-    number_to_currency filter.call.sum(:cash_in_cents)/10000
+    number_to_currency(filter.call.sum(:cash_in_cents)/10000 || 0)
+  end
+  
+  def total_difference
+    total = filter.call.decorate.map(&:unformatted_difference).reduce(:+)
+    number_to_currency(total)
+  end
+  
+  def total_cost
+    total = filter.call.decorate.map(&:unformatted_vehicle_cost).reduce(:+)
+    number_to_currency(total)
   end
 
   private
