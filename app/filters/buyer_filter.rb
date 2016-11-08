7e6471dcf335b3157382d003f93dc7192325d08e
GONZALO <gonzagaldamez@gmail.com>
2016-11-08 07:58:29 -0300

    Bug with dates

--- app/filters/buyer_filter.rb
+++ app/filters/buyer_filter.rb
@@ -5,8 +5,8 @@ class BuyerFilter
   def call
     buyers = Buyer.all
     buyers = buyers.where('first_name ilike ? OR last_name ilike ?', "%#{@name}%", "%#{@name}%") if @name.present?
-    buyers = buyers.where('created_at >= ?', @from) if @from.present?
-    buyers = buyers.where('created_at <= ?', @to) if @to.present?
+    buyers = buyers.where('created_at >= ?', @from.to_date) if @from.present?
+    buyers = buyers.where('created_at <= ?', @to.to_date) if @to.present?
     buyers
   end
 end
